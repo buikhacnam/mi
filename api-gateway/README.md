00bf4b4f35a9d6e9883e2964fc50712f27ac5cb0
Casey Bui <buikhacnam11@gmail.com>
2022-11-12 11:44:58 +0700

    security

---
+++ api-gateway/README.md
@@ -0,0 +1,7 @@
+Run keycloak docker:
+
+```
+docker run -p 8181:8080 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin quay.io/keycloak/keycloak:18.0.0 start-dev
+```
+
+Go to http://localhost:8181 and login with admin/admin
--- order-service/src/main/java/com/buinam/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/buinam/orderservice/controller/OrderController.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/orders")
+@RequestMapping("api/order")
 public class OrderController {
     @Autowired
     private OrderService orderService;
